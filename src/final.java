6606eaadb9ff58920c1b53bca496984b320ae231
Narek Benyaminyan <narek.benyaminyan@picsart.com>
2021-03-15 11:14:37 +0400

    final_version

---
+++ src/final.java
@@ -0,0 +1,29 @@
+public class first {
+    public static void main(String[] args) {
+        int num = 10;
+
+        String text = "Picsart";
+        String[] sym = new String[]{"-", ".", "|", "-"};
+        int heightBase = sym.length;
+        int height = 2 * heightBase + 1;
+        int widthBase = text.length() / 2 + 2;
+        int width = text.length() + 4;
+        for (int row = 0; row < height; row++) {
+            int xc = Math.abs(heightBase - row);
+            if (xc == 0) {
+                System.out.println("- " + text + " -");
+            } else {
+                for (int col = 0; col < width; col++) {
+                    int yc = Math.abs(widthBase - col);
+                    if (col < xc || col > (width - xc - 1) || (col + xc) % 2 == 0) {
+                        System.out.print(" ");
+                    } else {
+                        System.out.print(sym[sym.length - xc]);
+                    }
+                }System.out.println(" ");
+            }
+
+        }
+    }
+}
+
