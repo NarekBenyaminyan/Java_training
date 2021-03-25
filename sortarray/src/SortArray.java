8e9476a78e43160276921176e7394bcb181b3f35
Narek Benyaminyan <narek.benyaminyan@picsart.com>
2021-03-25 16:51:30 +0400

    finalversion

--- sortarray/src/SortArray.java
+++ sortarray/src/SortArray.java
@@ -2,14 +2,12 @@ public class SortArray {
     public static void main(String[] args) {
 
         int anArray[] = {12, 45, 67, 32, 6, 2, 6, 71, 67, 4, 2, 11, 6, 90, 780, 454};
-        int temp; // popoxakana vorov pahelu em met arjeqs, vor elementi korust chlini
-      boolean isSorted = false; // qani vor mi angama sortavorelu petqa , enqan pttvi minchev jist sortavori while@ dra hamara
-
+        int temp; 
+      boolean isSorted = false; 
         while (!isSorted) {
                  isSorted = true;
-            for (int i = 0; i < anArray.length - 1; i++) { // pttveluya masivis erkarutyamb u kangni, mi angama sortavorelu
-                if (anArray[i] > anArray[i + 1]) {
-                    temp = anArray[i + 1]; //arjeqneri veragrman hamara.
+            for (int i = 0; i < anArray.length - 1; i++) { 
+                    temp = anArray[i + 1]; 
                     anArray[i + 1] = anArray[i];
                     anArray[i] = temp;
                     isSorted = false;
@@ -19,7 +17,7 @@ public class SortArray {
             
 
 
-        for (int i = 0; i < anArray.length; i++) {  // elementner@ tpelu hamar
+        for (int i = 0; i < anArray.length; i++) {  
             System.out.print(anArray[i] + "  ");
         }
     }
